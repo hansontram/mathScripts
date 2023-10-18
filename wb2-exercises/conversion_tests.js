122283c1f2c55454443cdad0127b676142e24cf7
Hanson Tram <htram.dev@gmail.com>
2023-10-18 14:25:23 -0700

    Finish all exercises

---
+++ wb2-exercises/conversion_tests.js
@@ -0,0 +1,31 @@
+// Description: This scriopt tests various numeric conversion techniques
+// Author: Jordan Q. Newprogrammer
+
+let a = "  101.1  "
+let b = "55"
+let c = "402 Stevens"
+let d = "Number 5  "
+
+// Task 1
+console.log("parseInt", parseInt(a))
+console.log(parseInt(b))
+console.log(parseInt(c))
+console.log(parseInt(d))
+
+//Task 2
+console.log("parseFloat",parseFloat(a))
+console.log(parseFloat(b))
+console.log(parseFloat(c))
+console.log(parseFloat(d))
+
+//Task 3
+console.log("Number",Number(a))
+console.log(Number(b))
+console.log(Number(c))
+console.log(Number(d))
+
+//Task 4
+console.log("+",+a)
+console.log(+b)
+console.log(+c)
+console.log(+d)
