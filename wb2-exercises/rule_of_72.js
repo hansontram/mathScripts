8e9dd6b5ad20a8cdc60c823498d8fdefb546f2bb
Hanson Tram <htram.dev@gmail.com>
2023-10-18 10:37:21 -0700

    Add rule of 72 exercise

---
+++ wb2-exercises/rule_of_72.js
@@ -0,0 +1,6 @@
+let interestRate = 10;
+let savingsAccount = 10000
+let doubleBalance = savingsAccount * 2
+let years = 72 / interestRate
+
+console.log("At a " + interestRate + "% interest rate, your savings account will be worth " + doubleBalance + " in " + years)
