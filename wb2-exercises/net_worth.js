59d2f5e0b55c534125cba2a7abdfe556d2e4b658
Hanson Tram <htram.dev@gmail.com>
2023-10-18 10:10:28 -0700

    Finish netWorth exercises

--- wb2-exercises/net_worth.js
+++ wb2-exercises/net_worth.js
@@ -5,4 +5,12 @@ let assets = 30000
 let debt = 100
 
 let netWorth = assets - debt 
-console.log("Your net worth is " + networth)
+console.log("Your net worth is " + netWorth)
+
+// exercise 2: 
+let widthOfRectangle = 6
+let heightOfRectangle = 3
+let areaOfRectangle = widthOfRectangle * heightOfRectangle
+
+console.log("The area of the rectangle is " + areaOfRectangle)
+
