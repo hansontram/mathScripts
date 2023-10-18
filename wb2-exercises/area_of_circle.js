f161deaafec89f24bd33ab0826e85372c76ccc5b
Hanson Tram <htram.dev@gmail.com>
2023-10-18 10:23:49 -0700

    Add area of circle exercise

---
+++ wb2-exercises/area_of_circle.js
@@ -0,0 +1,4 @@
+let radius = 5
+let area = Math.PI * Math.pow(radius, 2)
+
+console.log("The area of a circle with radius " + radius + " is " + area)
---
+++ wb2-exercises/tip_amount.js
@@ -0,0 +1,4 @@
+let foodBill = 100
+let tip = foodBill * .20
+console.log(tip)
+console.log("The tip on a $ "+ foodBill + " food bill is $ " + tip)
