7779108bb2069170139a171424877ae14aa5b8ed
Hanson Tram <htram.dev@gmail.com>
2023-10-19 15:02:54 -0700

    Add more exercises

--- wb2-exercises/IfScripts/min_max.js
+++ wb2-exercises/IfScripts/min_max.js
@@ -19,10 +19,28 @@
 //     console.log(c + " is the largest value")
 // }
 
-// Exercise 2 
-let studentName = "John"
-let studentMajor = "CSCI"; 
+// Exercise 2
+let studentName = "John";
+let studentMajor = "MKT";
 
-if (studentMajor === "CSCI"){
-    console.log(`Student: ${studentName}, Major: Computer Science, Advising Location: Sheppard Hall, Room 314`)
+if (studentMajor === "CSCI") {
+  console.log(
+    `Student: ${studentName}, Major: Computer Science, Advising Location: Sheppard Hall, Room 314`
+  );
+} else if (studentMajor === "BIOL") {
+  console.log(
+    `Student: ${studentName}, Major: Biology, Advising Location: Science Bldg, Room 310`
+  );
+} else if (studentMajor === "ENG") {
+  console.log(
+    `Student: ${studentName}, Major: English, Advising Location: Kerr Hall, Room 201`
+  );
+} else if (studentMajor === "HIST") {
+  console.log(
+    `Student: ${studentName}, Major: History, Advising Location: Kerr Hall, Room 114`
+  );
+} else if (studentMajor === "MKT") {
+  console.log(
+    `Student: ${studentName}, Major: Marketing, Advising Location: Kerr Hall, Room 310`
+  );
 }
